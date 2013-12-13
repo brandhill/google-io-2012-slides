e0deb9293698de15212b038a01f658031297cc8a
Shih-Wei <brandhill.g@gmail.com>
2013-12-13 15:46:07 +0800

    完成第一版

--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'Hello, Everyone',
+    subtitle: '',
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
@@ -22,12 +22,8 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Hill Hung',
+    company: 'Senior Engineer<br>Accupass'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
